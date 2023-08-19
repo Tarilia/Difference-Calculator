49862bd33c31e62f981f87d5a52b8ac406401792
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2023-08-19 14:17:42 +0300

    update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -11,8 +11,10 @@ def get_info():
             )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',
+            help='set format of output')
     args = parser.parse_args()
-    print(args.first_file, args.second_file)
+    print(args.first_file, args.second_file, args.format)
 
 
 def main():
