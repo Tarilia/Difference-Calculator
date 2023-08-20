0f2ffd35f31bfaa11ab628f53995212f479fb27f
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2023-08-20 17:49:38 +0300

    update README.md

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Tarilia/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/Tarilia/python-project-50/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/b95d0aa401fb54de5c70/maintainability)](https://codeclimate.com/github/Tarilia/python-project-50/maintainability)
 
 
 #### JSON file comparison:
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,12 +10,12 @@ from gendiff.gendiff import generate_diff
 def get_args():
     parser = argparse.ArgumentParser(
             usage='gendiff [-h] first_file second_file',
-            description='Compares two configuration files and shows a difference.'
+            description='Compares two\
+                    configuration files and shows a difference.'
             )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format',
-            help='set format of output')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     return args
 
