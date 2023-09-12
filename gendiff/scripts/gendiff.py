d792cd2f852eebceca3c7b60a53ac99224d54d52
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2023-09-12 14:49:10 +0300

    fix lint

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,10 +9,9 @@ from gendiff.gendiff import generate_diff
 
 def get_args():
     parser = argparse.ArgumentParser(
-            usage='gendiff [-h] first_file second_file',
-            description='Compares two\
-                    configuration files and shows a difference.'
-            )
+        usage='gendiff [-h] first_file second_file',
+        description='Compares two\
+        configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', default='stylish',
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -11,7 +11,6 @@ def test_generate_diff():
     nested_file1_yml = 'tests/fixtures/nested_file1.yml'
     nested_file2_yml = 'tests/fixtures/nested_file2.yml'
 
-
     result_json = open('tests/fixtures/json_result.txt').read()
     result_yml = open('tests/fixtures/yml_result.txt').read()
     result_stylish_json = open('tests/fixtures/nested_result.txt').read()
@@ -19,18 +18,17 @@ def test_generate_diff():
     result_plain = open('tests/fixtures/plain_result.txt').read()
     nested_result_json = open('tests/fixtures/nested_json_result.txt').read()
 
-
     assert generate_diff(file1_json, file2_json) == result_json
     assert generate_diff(file1_yml, file2_yml) == result_yml
     assert generate_diff(nested_file1_json, nested_file2_json,
-        formatter='stylish') == result_stylish_json
+                         formatter='stylish') == result_stylish_json
     assert generate_diff(nested_file1_yml, nested_file2_yml,
-        formatter='stylish') == result_stylish_yml
+                         formatter='stylish') == result_stylish_yml
     assert generate_diff(nested_file1_json, nested_file2_json,
-        formatter='plain') == result_plain
+                         formatter='plain') == result_plain
     assert generate_diff(nested_file1_yml, nested_file2_yml,
-        formatter='plain') == result_plain
+                         formatter='plain') == result_plain
     assert generate_diff(nested_file1_json, nested_file2_json,
-        formatter='json') == nested_result_json
+                         formatter='json') == nested_result_json
     assert generate_diff(nested_file1_yml, nested_file2_yml,
-        formatter='json') == nested_result_json
+                         formatter='json') == nested_result_json
